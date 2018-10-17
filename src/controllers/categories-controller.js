4670bd26b308fab61010b5737f923198fa7b9cb5
Wayan Jimmy <jimmyeatcrab@gmail.com>
2018-10-17 14:03:48 +0800

    category search

--- src/controllers/categories-controller.js
+++ src/controllers/categories-controller.js
@@ -1,10 +1,20 @@
+const _ = require('lodash')
+
 const ex = require('../utils/express')
 const { Category } = require('../models')
 
 const index = ex.createRoute(async (req, res) => {
-  const data = await Category.query()
+  const keyword = _.defaultTo(req.query.search, '')
+
+  let categoryQuery = Category.query()
     .orderBy('created_at', 'ASC')
     .whereNull('deleted_at')
+
+  if (keyword.length > 0) {
+    categoryQuery = categoryQuery.where('name', 'ilike', `%${keyword}%`)
+  }
+
+  const data = await categoryQuery
 
   res.json({
     object: 'list',
