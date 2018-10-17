370eb165ec89f657e614818e775ca35394f3b7cd
Wayan Jimmy <jimmyeatcrab@gmail.com>
2018-10-18 00:23:58 +0800

    add input text component

--- frontend/src/routes/UserList.js
+++ frontend/src/routes/UserList.js
@@ -1,17 +1,29 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
 import React, { Component } from 'react'
+import { Formik } from 'formik'
 
 import Layout from '../views/Layout'
 import Paginator from '../containers/Paginator'
+import InputText from '../views/InputText'
+
+function initUser() {
+  return {
+    id: null,
+    name: '',
+    email: '',
+    password: ''
+  }
+}
 
 class UserList extends Component {
   state = {
-    search: ''
+    search: '',
+    user: initUser()
   }
 
   render() {
-    const { search } = this.state
+    const { search, user } = this.state
 
     return (
       <Layout>
@@ -55,7 +67,6 @@ class UserList extends Component {
                       <thead>
                         <tr>
                           <th>Nama</th>
-                          <th className="uk-text-center">Peran</th>
                           <th />
                         </tr>
                       </thead>
@@ -63,7 +74,6 @@ class UserList extends Component {
                         {items.map(user => (
                           <tr className="uk-visible-toggle" key={user.id}>
                             <td>{user.name}</td>
-                            <td className="uk-text-center">Test</td>
                             <td>
                               <a
                                 className="uk-icon-link uk-invisible-hover"
@@ -79,6 +89,83 @@ class UserList extends Component {
                       </tbody>
                     </table>
                     <Paginator.Pagination {...getPaginationProps()} />
+                  </div>
+                  <div className="uk-width-1-3@l uk-width-1-1">
+                    <Formik
+                      initialValues={{ ...user }}
+                      validate={values => {
+                        let errors = {}
+                        return errors
+                      }}
+                      render={({
+                        values,
+                        handleChange,
+                        handleSubmit,
+                        isSubmitting
+                      }) => (
+                        <form
+                          onSubmit={handleSubmit}
+                          className="uk-form-stacked"
+                        >
+                          <div className="uk-margin">
+                            <label htmlFor="name" className="uk-form-label">
+                              Nama
+                            </label>
+                            <div className="uk-form-controls">
+                              <InputText
+                                name="name"
+                                placeholder="Nama"
+                                value={values.name}
+                                onChange={handleChange}
+                              />
+                            </div>
+                          </div>
+                          <div className="uk-margin">
+                            <label htmlFor="email" className="uk-form-label">
+                              Email
+                            </label>
+                            <div className="uk-form-controls">
+                              <InputText
+                                name="email"
+                                placeholder="Email"
+                                value={values.email}
+                                onChange={handleChange}
+                              />
+                            </div>
+                          </div>
+                          <div className="uk-margin">
+                            <label className="uk-form-label" htmlFor="password">
+                              Sandi
+                            </label>
+                            <div className="uk-form-controls">
+                              <InputText
+                                name="password"
+                                type="password"
+                                placeholder="****"
+                                value={values.password || ''}
+                                onChange={handleChange}
+                              />
+                            </div>
+                          </div>
+                          <div className="uk-margin uk-flex uk-flex-between">
+                            <button
+                              className="uk-button uk-button-primary"
+                              type="submit"
+                              disabled={isSubmitting}
+                            >
+                              Simpan
+                            </button>
+                            <button
+                              className="uk-button uk-button-danger"
+                              type="button"
+                              onClick={() => {}}
+                            >
+                              Hapus
+                            </button>
+                          </div>
+                        </form>
+                      )}
+                    />
                   </div>
                 </div>
               )}
