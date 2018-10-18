cd00fe1dbf6bd9d691740eb7d8fee3caaae7337a
Wayan Jimmy <jimmyeatcrab@gmail.com>
2018-10-18 08:17:03 +0800

    distributors migration and seed

---
+++ backend/src/db/seeds/03-distributors.js
@@ -0,0 +1,15 @@
+const faker = require('faker')
+
+exports.seed = function(knex, _Promise) {
+  return knex('distributors')
+    .del()
+    .then(() => {
+      const distributors = Array.from({ length: 10 }).map(() => ({
+        name: faker.name.findName(),
+        created_at: new Date().toISOString(),
+        updated_at: new Date().toISOString()
+      }))
+
+      return knex('distributors').insert(distributors)
+    })
+}
