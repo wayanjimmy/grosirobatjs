fb46bbfb775ac1a4ffdd855ce7c0bdc4053ac7ce
Wayan Jimmy <jimmyeatcrab@gmail.com>
2018-10-21 20:34:04 +0800

    me endpoint using knex

--- backend/src/controllers/me-controller.js
+++ backend/src/controllers/me-controller.js
@@ -1,15 +1,26 @@
-const ex = require('../utils/express')
+const knex = require('knex')(require('../config/knexfile'))
+const joinJs = require('join-js').default
 
-const { User } = require('../models')
+const ex = require('../utils/express')
+const { NotFoundError } = require('../utils/db')
+const { relationMaps } = require('../utils/db')
+const { userTransformer } = require('../utils/transformers')
 
 const index = ex.createRoute(async (req, res) => {
   const { id } = req.payload.sub
 
-  const user = await User.query()
-    .findById(id)
-    .throwIfNotFound()
+  let users = await knex('users')
+    .select('*')
+    .whereNull('deleted_at')
+    .where('id', id)
 
-  res.json(User.transform(user))
+  if (users.length === 0) {
+    throw new NotFoundError('not found')
+  }
+
+  users = joinJs.mapOne(users, relationMaps, 'userMap', '')
+
+  res.json(userTransformer(users))
 })
 
 module.exports = {
